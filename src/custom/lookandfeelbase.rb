7aa3759e5f1b9976db1273986f5cecaaa69f20d4
Zeno R.R. Davatz <zdavatz@ywesee.com>
2012-03-15 15:20:36 +0100

    Updated language file

--- src/custom/lookandfeelbase.rb
+++ src/custom/lookandfeelbase.rb
@@ -17,18 +17,18 @@ module SHOE
     }
     DICTIONARIES = { # a-Z
       'de' => {
-        :article_title      => "Artikeln",
-        :explain_text       => "Willkommen zu Shoe Projekt",
+        :article_title      => "Artikel",
+        :explain_text       => "Willkommen beim Shoe Projekt",
         :foot_text          => "",
-        :head_text          => "Das ist gute shue :-D",
+        :head_text          => "Das ist ein guter Shoe :-D",
         :home_title         => "Home",
 				:navigation_divider	=> '&nbsp;|&nbsp;',
       },
       'en' => {
         :article_title      => "Articles",
-        :explain_text       => "Welcome to Shoe Project",
+        :explain_text       => "Welcome to the Shoe Project",
         :foot_text          => "",
-        :head_text          => "It's nice shoe :-D",
+        :head_text          => "This is a nice shoe :-D",
         :home_title         => "Home",
 				:navigation_divider	=> '&nbsp;|&nbsp;',
       }
